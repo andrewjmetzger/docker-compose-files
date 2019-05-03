b838622f1baa22c3fe10cecd8ddc068d6787c395
Andrew Metzger <andrewjmetzger@users.noreply.github.com>
2019-05-03 12:09:23 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-#  Read common.env
-export $(grep -v '^#' common.env | xargs -d '\n')
+#  Read common .env
+export $(grep -v '^#' .env | xargs -d '\n')
 
 # TODO: Do work here
 echo $COMMON_FOO
 
-# Unset variables from common.env
-unset $(grep -v '^#' common.env | sed -E 's/(.*)=.*/\1/' | xargs)
+# Unset variables from common .env
+unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs)
 exit
